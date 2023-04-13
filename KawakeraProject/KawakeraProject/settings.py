58cd6286dd015b371e8e62170e14e3106ecb5564
tf63 <tfukuda9031@gmail.com>
2023-04-13 09:02:29 +0000

    fix settings.py

--- KawakeraProject/KawakeraProject/settings.py
+++ KawakeraProject/KawakeraProject/settings.py
@@ -21,7 +21,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-trjuc577wc%8n3vy$lf=$xfm5__d4(0$((4tlt(qmxn7072lyh"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
