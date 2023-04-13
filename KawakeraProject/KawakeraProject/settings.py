fe5525530aa70dcd3d949136b8cb07247f4691c7
rabbit0624 <itoru03@gmail.com>
2023-04-13 10:21:49 +0000

    add_templates

--- KawakeraProject/KawakeraProject/settings.py
+++ KawakeraProject/KawakeraProject/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+# 静的ファイルへのパス
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
