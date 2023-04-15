5fed54a9b37f0dabb7f0df389a2562d04f08f4cd
kazukiyoda7 <yodakazu@icloud.com>
2023-04-15 10:35:23 +0000

    fix chat

--- KawakeraProject/MainApp/chat.py
+++ KawakeraProject/MainApp/chat.py
@@ -62,6 +62,55 @@ def chat_inference(context):
     return animal_name
 
 
+# 動物名から豆知識と食べ物，生態地域，豆知識を生成
+def chat_knowledge(animal_name):
+    system_setting = """\
+        ####Settings###
+        You are a scholar who knows a lot about animals. You have a wealth of knowledge about what animals eat, where they live, and trivia about them.
+
+        ######Status###.
+        You are about to be asked a question by a curious user about what animals eat, where they live, and trivia about animals.
+
+        ######Requirement###.
+        Please answer the user's questions.
+
+        Here are some examples
+
+        Example 1
+        user: What do lions eat?
+        assistant: They eat bamboo shoots and bamboo shoots as well as bamboo shoots and bamboo grass. It has been recorded that it sometimes catches and eats small animals such as insects.
+        user:Where do lions live?
+        assistant:Lions are found in Central Africa (excluding southern South Africa) and northwestern India.
+        user: Please tell us some trivia about lions.
+        assistant: Lions spend most of the day sleeping. Lions originally spend 15 to 20 hours a day sleeping or lying down to relax. The success rate of temporary hunting is 20% to 30%. Hunting is not done every day, but every few days. In zoos, fasting days are set aside in accordance with wild ecology, and food is given every few days.
+
+        Example 2
+        user: Please tell me what giant pandas eat.
+        assistant: They eat large mammals such as zebras, wild boars, gnus, and antelope.
+        user:Please tell us where the giant panda lives.
+        assistant: Giant pandas live in the mountainous forests of Sichuan, Shaanxi, and Gansu provinces in the southwestern part of the People's Republic of China, at altitudes ranging from 1300 to 3500 meters.
+        user: Please tell us some trivia about giant pandas.
+        assistant: Pandas are actually carnivores, and are classified in the Carnivora family. While herbivores have long intestines that are more than 20 times their body length, pandas' intestines are about 4 times their body length. From this point of view, pandas are carnivores. Pandas spend much of the day eating, but only 20% of the bamboo and bamboo grass they eat can be digested. Their eyes are surprisingly sharp and scary. They eat all day long.
+        """
+    chatgpt = Chatgpt(system_setting)
+    question_food = f"Tell us what {animal_name} eat."
+    chatgpt.input_message(question_food)
+    food = chatgpt.input_list[-1]["content"]
+    question_area = f"Tell us where the {animal_name} live."
+    chatgpt.input_message(question_area)
+    area = chatgpt.input_list[-1]["content"]
+    question_trivia = f"Tell me some trivia about {animal_name}"
+    chatgpt.input_message(question_trivia)
+    trivia = chatgpt.input_list[-1]["content"]
+    knowledge = {
+        "animal_name": animal_name,
+        "food": food,
+        "area": area,
+        "trivia": trivia,
+    }
+    return knowledge
+
+
 # 動物名から食べるものを生成
 def chat_food(animal_name):
     system_setting = """\
@@ -146,55 +195,6 @@ def chat_trivia(animal_name):
     chatgpt.input_message(question)
     trivia = chatgpt.input_list[-1]["content"]
     return trivia
-
-
-# 動物名から豆知識と食べ物，生態地域，豆知識を生成
-def chat_knowledge(animal_name):
-    system_setting = """\
-        ####Settings###
-        You are a scholar who knows a lot about animals. You have a wealth of knowledge about what animals eat, where they live, and trivia about them.
-
-        ######Status###.
-        You are about to be asked a question by a curious user about what animals eat, where they live, and trivia about animals.
-
-        ######Requirement###.
-        Please answer the user's questions.
-
-        Here are some examples
-
-        Example 1
-        user: What do lions eat?
-        assistant: They eat bamboo shoots and bamboo shoots as well as bamboo shoots and bamboo grass. It has been recorded that it sometimes catches and eats small animals such as insects.
-        user:Where do lions live?
-        assistant:Lions are found in Central Africa (excluding southern South Africa) and northwestern India.
-        user: Please tell us some trivia about lions.
-        assistant: Lions spend most of the day sleeping. Lions originally spend 15 to 20 hours a day sleeping or lying down to relax. The success rate of temporary hunting is 20% to 30%. Hunting is not done every day, but every few days. In zoos, fasting days are set aside in accordance with wild ecology, and food is given every few days.
-
-        Example 2
-        user: Please tell me what giant pandas eat.
-        assistant: They eat large mammals such as zebras, wild boars, gnus, and antelope.
-        user:Please tell us where the giant panda lives.
-        assistant: Giant pandas live in the mountainous forests of Sichuan, Shaanxi, and Gansu provinces in the southwestern part of the People's Republic of China, at altitudes ranging from 1300 to 3500 meters.
-        user: Please tell us some trivia about giant pandas.
-        assistant: Pandas are actually carnivores, and are classified in the Carnivora family. While herbivores have long intestines that are more than 20 times their body length, pandas' intestines are about 4 times their body length. From this point of view, pandas are carnivores. Pandas spend much of the day eating, but only 20% of the bamboo and bamboo grass they eat can be digested. Their eyes are surprisingly sharp and scary. They eat all day long.
-        """
-    chatgpt = Chatgpt(system_setting)
-    question_food = f"Tell us what {animal_name} eat."
-    chatgpt.input_message(question_food)
-    food = chatgpt.input_list[-1]["content"]
-    question_area = f"Tell us where the {animal_name} live."
-    chatgpt.input_message(question_area)
-    area = chatgpt.input_list[-1]["content"]
-    question_trivia = f"Tell me some trivia about {animal_name}"
-    chatgpt.input_message(question_trivia)
-    trivia = chatgpt.input_list[-1]["content"]
-    knowledge = {
-        "animal_name": animal_name,
-        "food": food,
-        "area": area,
-        "trivia": trivia,
-    }
-    return knowledge
 
 
 # contextを受け取って動物を推論し生態と豆知識を返す関数(OpenAIからエラーが帰ってくる．多分アクセス過多)
