51c7fd4a94fa3f1184d5a85859bd753fad3b6557
ultimatemagic79 <sea.starry.ks@gmail.com>
2023-04-15 15:38:39 +0900

    add replicate

--- KawakeraProject/MainApp/clip.py
+++ KawakeraProject/MainApp/clip.py
@@ -1,7 +1,9 @@
 import replicate
 
 img = "/docs/img/dal.jpg"
-def create_prompt(img)
+
+
+def create_prompt(img):
     text = replicate.run(
         "methexis-inc/img2prompt:50adaf2d3ad20a6f911a8a9e3ccf777b263b8596fbd2c8fc26e8888f8a0edbb5",
         input={"image": open(img, "rb")},
@@ -9,6 +11,7 @@ def create_prompt(img)
     first_comma_index = text.find(",")
     output = text[:first_comma_index]
     return output
+
 
 if __name__ == "__main__":
     output = create_prompt(img)
