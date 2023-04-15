f6ece8646a1605857d55e588ef69b1cacc85ef88
kazukiyoda7 <yodakazu@icloud.com>
2023-04-15 09:51:54 +0000

    add prompt

--- KawakeraProject/MainApp/chat.py
+++ KawakeraProject/MainApp/chat.py
@@ -32,7 +32,29 @@ class Chatgpt:
 # contextからなんの動物か推論する
 def chat_inference(context):
     # Chatgptにシステム設定のためのプロンプト
-    system_setting = ""
+    system_setting = """\
+        ####Settings###
+        You are a scholar who knows animals.
+
+        ####Situation###
+        You receive a text from a user that describes an image of an animal
+
+        ####Request###
+        Please use the following format in your response
+        [name]: "animal name"
+        Please determine the animal most likely to be in the image based on the text you received from the user, step by step.
+        Please output the name of the animal you have determined according to the format.
+
+        Here are some examples
+
+        Example 1
+        user:a medium-sized dog breed with a short, white coat covered in black spots
+        assistant: [name] Dalmatian
+
+        Example 2
+        user: a black and white dog standing on top of a grass covered field, a picture by Toyen
+        assistant: [name] Shiba Inu
+        """
     # Chatgptクラスのインスタンス
     chatgpt = Chatgpt(system_setting)
     chatgpt.input_message(context)
@@ -42,27 +64,86 @@ def chat_inference(context):
 
 # 動物名から食べるものを生成
 def chat_food(animal_name):
-    system_setting = ""
+    system_setting = """\
+        ####Settings###
+        You are a scholar who knows animals.
+        
+        ####Situation###.
+        A question from a user who is curious about animals about what the animals eat
+
+        ####Request###.
+        Please answer the user's question.
+        Here are some examples
+
+        Example 1
+        user: Tell us where the lion live.
+        assistant: Eats large mammals such as zebras, wild boars, gnus, antelopes, etc.
+
+        Example 2
+        user: Tell us where the giant panda live.
+        assistant: They eat bamboo shoots as well as bamboo shoots and bamboo shoots. It has been recorded that they sometimes catch and eat small animals such as insects.
+        """
     chatgpt = Chatgpt(system_setting)
-    chatgpt.input_message(animal_name)
+    question = "Tell us where the {animal_name} live."
+    chatgpt.input_message(question)
     food = chatgpt.input_list[-1]["content"]
     return food
 
 
 # 動物名から生態地域を生成
 def chat_area(animal_name):
-    system_setting = ""
+    system_setting = """\
+        ####Settings###
+        You are a scholar who knows animals.
+        
+        ####Situation###.
+        A question from a user who is curious about animals about what the animals eat
+
+        ####Request###.
+        Please answer the user's question.
+        Here are some examples
+
+        Example 1
+        user: Tell us what lion eat.
+        assistant: Eats large mammals such as zebras, wild boars, gnus, antelopes, etc.
+
+        Example 2
+        user: Tell us what giant panda eat.
+        assistant: They eat bamboo shoots as well as bamboo shoots and bamboo shoots. It has been recorded that they sometimes catch and eat small animals such as insects.
+        """
     chatgpt = Chatgpt(system_setting)
-    chatgpt.input_message(animal_name)
+    question = "Tell us what {animal_name} eat."
+    chatgpt.input_message(question)
     area = chatgpt.input_list[-1]["content"]
     return area
 
 
 # 動物名から豆知識を生成
 def chat_trivia(animal_name):
-    system_setting = ""
+    system_setting = r"""\
+        ####Settings###
+        You are a scholar who knows animals.
+
+        ####Situation###.
+        A user is curious about animals and asks you about animal trivia
+
+        ####Requirement###
+        Please answer the user's question.
+        Here are some examples
+
+        Example 1
+        user: Tell me some trivia about lions.
+        assistant: Lions spend most of the day sleeping. Lions originally spend 15 to 20 hours a day sleeping or lying down to relax. The success rate of temporary hunting is 20% to 30%. Hunting is not done every day, but every few days. In zoos, fasting days are set aside in accordance with wild ecology, and food is given every few days.
+
+        Example 2
+        user: Tell me some trivia about giant pandas.
+        assistant: Pandas are actually carnivores, and are classified in the Carnivora family. While herbivores have long intestines that are more than 20 times their body length, 
+        the panda's intestines are about 4 times its body length. From this point of view, pandas are carnivores. Pandas spend much of the day eating, but only 20% of 
+        the bamboo and bamboo grass they eat can be digested. Their eyes are surprisingly sharp and scary. They eat all day long.
+        """
     chatgpt = Chatgpt(system_setting)
-    chatgpt.input_message(animal_name)
+    question = "Tell me some trivia about {animal_name}"
+    chatgpt.input_message(question)
     trivia = chatgpt.input_list[-1]["content"]
     return trivia
 
