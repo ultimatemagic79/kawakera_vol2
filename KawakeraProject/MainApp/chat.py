0110c89844d08108d7304d07d43e3d0bebb71127
kazukiyoda7 <yodakazu@icloud.com>
2023-04-16 05:12:34 +0000

    fix chat

--- KawakeraProject/MainApp/chat.py
+++ KawakeraProject/MainApp/chat.py
@@ -75,7 +75,7 @@ def chat_knowledge(animal_name):
     chatgpt.input_message(question_area)
     area = chatgpt.input_list[-1]["content"]
     # question_trivia = f"Tell me some trivia about {animal_name}"
-    question_trivia = f"tell me some interesting tidbits about {animal_name}, The output should be bulleted and each sentence should end with an '\n'."
+    question_trivia = f"tell me some interesting tidbits about {animal_name}. Please output only bullet points as the output needs to be concise. The output should be bulleted and each sentence should end with an '\n'."
     chatgpt.input_message(question_trivia)
     trivia = chatgpt.input_list[-1]["content"]
     knowledge = {
